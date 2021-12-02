ba5210c266cbf65fc2914366adaccbcb235fe1a2
Samuel Hunter <samuel@shunter.xyz>
2021-12-01 21:37:16 -0800

    Refactor Day 02

--- day02.lisp
+++ day02.lisp
@@ -1,48 +1,37 @@
 (defpackage #:advent2021.day02
-  (:use #:cl #:alexandria #:advent2021.util)
+  (:use #:cl #:alexandria #:split-sequence #:advent2021.util)
   (:export #:solve-part-1 #:solve-part-2))
 
 (in-package #:advent2021.day02)
 
 
 
-(defparameter +input+ (read-puzzle-sexp))
+(defparameter +input+
+  (parse-lines
+    (lambda (line)
+      (destructuring-bind (cmd arg) (split-sequence #\Space line)
+        (cons (intern (string-upcase cmd) :keyword)
+              (parse-integer arg))))))
 
-(defparameter +input+ '(
-                        (forward 5)
-                        (down 5)
-                        (forward 8)
-                        (up 3)
-                        (down 8)
-                        (forward 2)))
+(defun solve-part-1 ()
+  (loop :with breadth := 0
+        :with depth := 0
+        :for (cmd . mag) :in +input+
+        :do (ecase cmd
+              (:down (incf depth mag))
+              (:up (decf depth mag))
+              (:forward (incf breadth mag)))
+        :finally (return (* depth breadth))))
 
 (defun solve-part-2 ()
-  (loop :with horiz := 0
+  (loop :with aim := 0
+        :with breadth := 0
         :with depth := 0
-        :with aim := 0
-        :for (cmd mag) :in +input+
-        :do (cond
-              ((eq cmd 'forward) (incf horiz mag)
-                                 (incf depth (* aim mag))
-                                 )
-              ((eq cmd 'backward) (decf horiz mag))
-              ((eq cmd 'up)
-                            (decf aim mag))
-              ((eq cmd 'down)
-                              (incf aim mag)))
-        :do (print (list horiz depth aim))
-        :finally (return (* horiz depth))))(defun solve-part-1 ()
-
-  (loop :with horiz := 0
-        :with depth := 0
-        :for (cmd mag) :in +input+
-        :do (cond
-              ((eq cmd 'forward) (incf horiz mag))
-              ((eq cmd 'backward) (decf horiz mag))
-              ((eq cmd 'up) (incf depth mag))
-              ((eq cmd 'down) (decf depth mag)))
-        :finally (return (* horiz depth))))
-
-(defun solve-part-2 ()
-  )
-
+        :for (cmd . mag) :in +input+
+        :do (ecase cmd
+              (:down (incf aim mag))
+              (:up (decf aim mag))
+              (:forward
+                (incf breadth mag)
+                (incf depth (* aim mag))))
+        :finally (return (* depth breadth))))
